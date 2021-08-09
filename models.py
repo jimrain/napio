115991ea7a84851dfbb9d48d60d81f6c7fe41373
jimrain <jim@rainville.net>
2021-08-09 06:14:24 -0700

    Beautifying the object names in the admin.

--- models.py
+++ models.py
@@ -4,14 +4,21 @@ from django.db import models
 class PlaylistInfo(models.Model):
     name = models.CharField(max_length=32, default="")
 
+    class Meta:
+        verbose_name_plural = "Playlist Info"
+
     def __str__(self):
         return self.name
+
 
 
 class ArtistInfo(models.Model):
     name = models.CharField(max_length=32, default="")
     url = models.CharField(max_length=200)
     playlist = models.ManyToManyField(PlaylistInfo, related_name='artists')
+
+    class Meta:
+        verbose_name_plural = "Artist Info"
 
     def __str__(self):
         return self.name
@@ -21,6 +28,9 @@ class AlbumInfo(models.Model):
     name = models.CharField(max_length=32, default="")
     url = models.CharField(max_length=200)
     playlist = models.ManyToManyField(PlaylistInfo, related_name='albums')
+
+    class Meta:
+        verbose_name_plural = "Album Info"
 
     def __str__(self):
         return self.name
